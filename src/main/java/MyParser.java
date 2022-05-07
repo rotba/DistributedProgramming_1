f79598ff30e4a82fd0789d74fcc89f18e6f49d0d
Ubuntu <rotba@post.bgu.ac.il>
2022-05-07 11:51:33 +0000

    added -sentence newline in the LexicalizedParserargs

--- src/main/java/MyParser.java
+++ src/main/java/MyParser.java
@@ -18,7 +18,7 @@ public class MyParser {
 
     public static String parse(String taskType, String inPath) throws MyParserException {
         try{
-            LexicalizedParser.main(new String[]{"-retainTMPSubcategories","-writeOutputFiles", "-maxLength","60","-outputFormat", parsersLexicalParser.get(taskType), "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz", inPath});
+            LexicalizedParser.main(new String[]{"-retainTMPSubcategories","-writeOutputFiles", "-maxLength","60", "-sentences", "newline", "-outputFormat", parsersLexicalParser.get(taskType), "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz", inPath});
             return inPath+".stp";
         } catch (Exception e){
             System.err.println("MyParser: GenExc");
